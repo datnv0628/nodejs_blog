e5475c7e2905ecc2dc34731b886fa870cfadc50c
datnv0628 <nguyenvandat0628@gmail.com>
2025-08-30 14:57:33 +0700

    Init

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 const express = require("express");
-const morgan = require("morgan");
 const { engine } = require("express-handlebars");
 const path = require("path");
-
 const app = express();
-const port = 3001;
+const port = 3000;
 
-// Static file
-app.use(express.static(path.join(__dirname, "public")));
-
-// HTTP logger
-app.use(morgan("combined"));
-
-// Template engine
 app.engine(
   "hbs",
   engine({
@@ -25,6 +16,11 @@ app.set("views", path.join(__dirname, "resources/views"));
 app.get("/", (req, res) => {
   res.render("home");
 });
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+
+app.get("/news", (req, res) => {
+  res.send("News Pages");
+});
+
+app.listen(port, () => {
+  console.log(`Listening http://localhost:${port}`);
+});
